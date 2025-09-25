9fa3f3fe09179cd2abe1c46217294796b4da5255
skymidy <sky3fan@gmail.com>
2025-09-25 19:28:55 +1000

    Add spring-boot-starter-web and hello word get endpoint

--- src/main/java/com/tutorial/BootApplication.java
+++ src/main/java/com/tutorial/BootApplication.java
@@ -2,12 +2,19 @@ package com.tutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootApplication.class, args);
 	}
 
+    @GetMapping("/")
+    public String helloWorld(){
+        return "Hello Spring Boot";
+    }
 }
