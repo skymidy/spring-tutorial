5800e8cd92f22edef360c11b2d548e2faec38aaf
skymidy <sky3fan@gmail.com>
2025-11-13 22:02:27 +1000

    Add Create and Remove of CRUD operations to AuthorityRepository

--- src/main/java/com/tutorial/repository/AuthorityRepository.java
+++ src/main/java/com/tutorial/repository/AuthorityRepository.java
@@ -1,17 +1,33 @@
 package com.tutorial.repository;
 
+import com.tutorial.Enum.AuthorityEnum;
 import com.tutorial.model.entity.Authority;
 import com.tutorial.model.entity.AuthorityId;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface AuthorityRepository extends Repository<Authority, AuthorityId> {
 
-  Authority save(Authority role);
+    Authority save(Authority role);
 
-  Optional<Authority> findByUsername(String username);
+    Optional<Authority> findByUsername(String username);
 
-  Optional<Authority> findByAuthority(String authority);
+    Optional<Authority> findByAuthority(String authority);
+
+    @Modifying
+    @Query("INSERT INTO Authority (username, authority) VALUES (:username, :authority)")
+    void addAuthorityToUser(@Param("username") String username, @Param("authority") AuthorityEnum authority);
+
+    @Modifying
+    @Query("DELETE FROM Authority WHERE username = :username AND authority = :authority")
+    void removeAuthorityFromUser(@Param("username") String username, @Param("authority") AuthorityEnum authority);
+
+    @Modifying
+    @Query("DELETE FROM Authority WHERE username = :username")
+    void removeAllAuthoritiesFromUser(@Param("username") String username);
 }
