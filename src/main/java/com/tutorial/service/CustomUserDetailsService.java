1699270e4577f0fc469c84bc4707ed7670721a9f
skymidy <sky3fan@gmail.com>
2025-11-20 14:37:53 +1000

    Change name

--- src/main/java/com/tutorial/service/CustomUserDetailsService.java
+++ src/main/java/com/tutorial/service/CustomUserDetailsService.java
@@ -15,7 +15,7 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class SpingTutorialUserDetailsService implements UserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
