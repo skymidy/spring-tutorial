cf24aa604c767b92e25bbf044602fbba7c471216
skymidy <sky3fan@gmail.com>
2025-11-20 21:08:01 +1000

    Update PasswordService: - Use passwordDto as password carrier - remove authorization check from service layer

--- src/main/java/com/tutorial/service/PasswordService.java
+++ src/main/java/com/tutorial/service/PasswordService.java
@@ -4,12 +4,11 @@ import com.tutorial.Enum.AuthorityEnum;
 import com.tutorial.Enum.ErrorCodesEnum;
 import com.tutorial.exceptions.PasswordServiceException;
 import com.tutorial.mapper.UserMapper;
+import com.tutorial.model.dto.PasswordDto;
 import com.tutorial.model.dto.UserDto;
 import com.tutorial.model.entity.User;
 import com.tutorial.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import org.springframework.stereotype.Service;
@@ -30,18 +29,12 @@ public class PasswordService {
         this.passwordEncoder = passwordEncoder;
     }
 
-    private static final AuthorityEnum ADMIN_AUTHORITY = AuthorityEnum.ADMIN;
-
-    public UserDto updateCurrentUserPassword(String newPassword, UserDetails currentUser){
-        return updatePassword(currentUser.getUsername(), newPassword, currentUser);
-    }
-
     /**
      * Update user password.
      * TODO: Could be faster with optimised SQL queries in repository
      */
     @Transactional
-    public UserDto updatePassword(String username, String newPassword, UserDetails currentUser) {
+    public UserDto updatePassword(String username, PasswordDto passwordDto) {
 
         User user = userRepository
                 .findByUsername(username)
@@ -49,30 +42,14 @@ public class PasswordService {
                         () -> new PasswordServiceException(ErrorCodesEnum.USER_NOT_FOUND)
                 );
 
-        if (isTargetUserSameOrAdmin(username, currentUser)
-        ) {
-            log.warn("Non-admin attempted to update user id={}", username);
-            throw new PasswordServiceException(ErrorCodesEnum.ACCESS_DENIED);
-        }
-
-        if(passwordEncoder.matches(newPassword, user.getPassword())) {
+        if(passwordEncoder.matches(passwordDto.getPassword(), user.getPassword())) {
             throw new PasswordServiceException(ErrorCodesEnum.UNACCEPTABLE_PASSWORD, "New password must be different from the current password");
         }
 
-        user.setPassword(passwordEncoder.encode(newPassword));
+        user.setPassword(passwordEncoder.encode(passwordDto.getPassword()));
         User saved = userRepository.save(user);
 
         log.info("Updated user password username={}", saved.getUsername());
         return userMapper.toDto(saved);
-    }
-
-    private static boolean isTargetUserSameOrAdmin(String username, UserDetails currentUser) {
-        return !currentUser
-                .getAuthorities()
-                .contains(
-                        new SimpleGrantedAuthority(AuthorityEnum.ADMIN.toString())
-                )
-                ||
-                !currentUser.getUsername().equalsIgnoreCase(username);
     }
 }
