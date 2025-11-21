a58d41b52ba7d07c6185f923e6406a27326a1918
skymidy <sky3fan@gmail.com>
2025-11-21 13:12:25 +1000

    Updated api recourse repository and mapper

--- src/main/java/com/tutorial/mapper/ApiResourceMapper.java
+++ src/main/java/com/tutorial/mapper/ApiResourceMapper.java
@@ -6,6 +6,10 @@ import org.mapstruct.Mapping;
 import com.tutorial.model.dto.ApiResourceDto;
 import com.tutorial.model.entity.ApiResource;
 
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Mapper(componentModel = "spring")
 public interface ApiResourceMapper {
 
@@ -13,7 +17,12 @@ public interface ApiResourceMapper {
   @Mapping(source = "authenticationType.name", target = "authenticationTypeName")
   ApiResourceDto toDto(ApiResource entity);
 
+  @Mapping(target = "owner", ignore = true)
   @Mapping(source = "authenticationTypeId", target = "authenticationType.id")
   @Mapping(target = "id", ignore = true)
   ApiResource toEntity(ApiResourceDto dto);
+
+  default Set<ApiResourceDto> toDtoSet(Collection<ApiResource> entities){
+      return entities.stream().map(this::toDto).collect(Collectors.toSet());
+  }
 }
--- src/main/java/com/tutorial/repository/ApiResourceRepository.java
+++ src/main/java/com/tutorial/repository/ApiResourceRepository.java
@@ -3,20 +3,22 @@ package com.tutorial.repository;
 import com.tutorial.model.entity.ApiResource;
 
 // import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
-public interface ApiResourceRepository extends Repository<ApiResource, Integer> {
-    ApiResource save(ApiResource resource);
-    Optional<ApiResource> findById(Integer id);
+public interface ApiResourceRepository extends JpaRepository<ApiResource, Integer> {
 
-    List<ApiResource> findAll();
+    Set<ApiResource> findAllByIsEnabledTrue();
 
-    List<ApiResource> findByOwner_Username(String username);
+    Set<ApiResource> findByOwner_Username(String username);
 
     Optional<ApiResource> findByNameAndIsEnabledTrue(String name);
+    Optional<ApiResource> findByName(String name);
 
-    void deleteById(Integer id);
+    long deleteById(Integer id);      // count deleted rows
+    long deleteByName(String name);   // count deleted rows
 }
