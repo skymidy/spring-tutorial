f1f673959d1e058625f88c4c901f9f254576769b
skymidy <sky3fan@gmail.com>
2025-11-22 13:45:42 +1000

    Add RateLimitService

---
+++ src/main/java/com/tutorial/service/RateLimitService.java
@@ -0,0 +1,37 @@
+package com.tutorial.service;
+
+import com.tutorial.Enum.ErrorCodesEnum;
+import com.tutorial.exceptions.RateLimitServiceException;
+import com.tutorial.mapper.UserMapper;
+import com.tutorial.model.dto.UserDto;
+import com.tutorial.model.entity.User;
+import com.tutorial.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RateLimitService {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public RateLimitService(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+
+    public UserDto updateUserRateLimit(String username, Long rateLimit){
+
+        User user = userRepository
+                .findByUsername(username)
+                .orElseThrow(
+                        () -> new RateLimitServiceException(ErrorCodesEnum.USER_NOT_FOUND)
+                );
+
+        if(rateLimit <= 0) {
+            throw new RateLimitServiceException(ErrorCodesEnum.UNACCEPTABLE_RATE_LIMIT_VALUE, "RateLimit value zer or below unacceptable");
+        }
+
+        user.setRateLimit(rateLimit);
+
+        return userMapper.toDto(userRepository.save(user));
+    }
+}
