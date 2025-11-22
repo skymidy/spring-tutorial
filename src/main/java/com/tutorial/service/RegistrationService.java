7aa37c38648b712826480decc9356d0f3dca1b1b
skymidy <sky3fan@gmail.com>
2025-11-22 17:21:35 +1000

    Update of RegistrationService

--- src/main/java/com/tutorial/service/RegistrationService.java
+++ src/main/java/com/tutorial/service/RegistrationService.java
@@ -3,7 +3,9 @@ package com.tutorial.service;
 import com.tutorial.Enum.AuthorityEnum;
 import com.tutorial.Enum.ErrorCodesEnum;
 import com.tutorial.exceptions.RegistrationServiceException;
+import com.tutorial.mapper.AuthorityMapper;
 import com.tutorial.mapper.UserMapper;
+import com.tutorial.model.dto.AuthorityDto;
 import com.tutorial.model.dto.RegistrationRequestDto;
 import com.tutorial.model.dto.UserDto;
 import com.tutorial.model.entity.User;
@@ -14,6 +16,9 @@ import jakarta.transaction.Transactional;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Service
 public class RegistrationService {
 
@@ -23,15 +28,22 @@ public class RegistrationService {
     private final PasswordEncoder passwordEncoder;
     private final ApiKeyService apiKeyService;
     private final RateLimitService rateLimitService;
+    private final AuthorityMapper authorityMapper;
+
+
+    private final Set<AuthorityEnum> BASE_USER_AUTHORITIES = Set.of(AuthorityEnum.USER);
+    private final Long BASE_USER_RATE_LIMIT = 60L;
+
 
     public RegistrationService(UserRepository userRepository, AuthorityRepository authorityRepository,
-                               PasswordEncoder passwordEncoder, UserMapper userMapper, ApiKeyService apiKeyService, RateLimitService rateLimitService) {
+                               PasswordEncoder passwordEncoder, UserMapper userMapper, ApiKeyService apiKeyService, RateLimitService rateLimitService, AuthorityMapper authorityMapper) {
         this.userRepository = userRepository;
         this.authorityRepository = authorityRepository;
         this.passwordEncoder = passwordEncoder;
         this.userMapper = userMapper;
         this.apiKeyService = apiKeyService;
         this.rateLimitService = rateLimitService;
+        this.authorityMapper = authorityMapper;
     }
 
     @Transactional
@@ -47,11 +59,17 @@ public class RegistrationService {
                 new User(req.getUsername(), passwordEncoder.encode(req.getPassword()))
         );
 
-        authorityRepository.addAuthorityToUser(saved.getUsername(), AuthorityEnum.USER);
+        authorityRepository.saveAll(
+                authorityMapper.toEntitySet(
+                        saved.getUsername(),
+                        new AuthorityDto(BASE_USER_AUTHORITIES.stream().map(Enum::name).collect(Collectors.toSet()))
+                )
+        );
+
 
         apiKeyService.generateApiKeyForUser(saved.getUsername());
 
-        rateLimitService.updateUserRateLimit(saved.getUsername(), 60L);
+        rateLimitService.updateUserRateLimit(saved.getUsername(), BASE_USER_RATE_LIMIT);
 
         return userMapper.toDto(saved);
     }
