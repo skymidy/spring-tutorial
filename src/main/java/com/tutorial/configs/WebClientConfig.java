e9eca2fe00b8e07e4b046d781bd52e0c91a87147
skymidy <sky3fan@gmail.com>
2025-11-25 10:48:08 +1000

    Add timeouts for response and connection for webclient

--- src/main/java/com/tutorial/configs/WebClientConfig.java
+++ src/main/java/com/tutorial/configs/WebClientConfig.java
@@ -1,8 +1,14 @@
 package com.tutorial.configs;
 
+import io.netty.channel.ChannelOption;
+import reactor.netty.http.client.HttpClient;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.time.Duration;
 
 
 @Configuration
@@ -11,7 +17,12 @@ public class WebClientConfig {
     @Bean
     public WebClient webClient() {
         return WebClient.builder()
-                .build(); // No base URL
+                .clientConnector(new ReactorClientHttpConnector(
+                        HttpClient.create()
+                                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5_000) // 5 sec connect
+                                .responseTimeout(Duration.ofSeconds(30))             // 30 sec response
+                ))
+                .build();
     }
 }
 
